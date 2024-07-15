fd327fe2b7e74c5ce123e90db06bfe71e61d01be
subtanjiroo <138212507+subtanjiroo@users.noreply.github.com>
2024-07-15 14:10:53 +0700

    Delete backend directory

--- backend/routes/order.js
+++
@@ -1,9 +0,0 @@
-// const express = require("express");
-// const {
-//   addOrder,
-//   getOrder,
-// } = require("../controller/order_movie_service/order_service");
-// const router = express.Router();
-// router.get("/getOrder", getOrder);
-// router.post("/addOrder", addOrder);
-// module.exports = router;
