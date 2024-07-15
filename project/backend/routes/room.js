156f6faf754fa83b3bbabc62ce1063ab194156b2
subtanjiroo <138212507+subtanjiroo@users.noreply.github.com>
2024-07-15 13:41:07 +0700

    Delete project directory

--- project/backend/routes/room.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const {
-  addRoom,
-  getRoom,
-} = require("../controller/order_movie_service/addRoom");
-const router = express.Router();
-router.get("/getR", getRoom);
-router.post("/addR", addRoom);
-module.exports = router;
