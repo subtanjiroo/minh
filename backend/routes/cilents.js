614befb1cfd297d922000b8947e73ac7c615e437
subtanjiroo <138212507+subtanjiroo@users.noreply.github.com>
2024-07-15 13:40:41 +0700

    Delete backend directory

--- backend/routes/cilents.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Client = require("../controller/order_movie_service/cilentIF");
-const router = express.Router();
-
-router.post("/login", Client.loginClient);
-router.post("/register", Client.registerClient);
-
-module.exports = router;
--- backend/routes/logOrder.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getLO, addLO } = require("../controller/order_movie_service/addLO");
-
-const router = express.Router();
-
-router.get("/getLO", getLO);
-router.post("/addLO", addLO);
-
-module.exports = router;
